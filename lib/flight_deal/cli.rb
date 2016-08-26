effe0d23db95619040b3c2d335e205665efb3413
Minh <mhnguyen289@gmail.com>
2016-08-25 17:57:05 -0700

    finishing touches and added comments for clarification

--- lib/flight_deal/cli.rb
+++ lib/flight_deal/cli.rb
@@ -1,23 +1,25 @@
 #CLI Controller
+
 class FlightDeal::CLI
 
 	def call
 		puts "Welcome to FlightDeal!"
 		FlightDeal::Scraper.new.scrape_cities
+
 		choose_city
 	end
 
 	def list_cities
 		puts "Choose a city to find Flight Deals"
 		puts "----------------------------------------------------------------"
-		FlightDeal::City.all.each_with_index do |city, index|
+		all_cities = FlightDeal::City.all.each_with_index do |city, index|
 			puts "#{index + 1}. #{city.name}"
 		end
 		puts "----------------------------------------------------------------"
 	end
 
 	def choose_city
-		city_choice = nil 
+		city_choice = nil
 		until city_choice == 'exit'
 			list_cities
 			puts "Enter the number for more info or type exit to leave program"
@@ -26,9 +28,8 @@ class FlightDeal::CLI
 				puts "Now exiting the program. See you later!"
 				
 			elsif city_choice.to_i  <= FlightDeal::City.all.size
-
 				city = FlightDeal::City.all[city_choice.to_i - 1]
-				@city_deals = FlightDeal::Scraper.new.scrape_deals(city) #create a new scarper & pull deals for that city
+				@city_deals = FlightDeal::Scraper.new.scrape_deals(city) #pulls deals for that city
 				puts "here are the recent deals from #{city.name}"
 				puts "----------------------------------------------------------------"
 				@city_deals.each_with_index do |deal, index|
@@ -43,8 +44,7 @@ class FlightDeal::CLI
 	end
 
 	def choose_deal
-		
-			deal_choice = nil 
+			deal_choice = nil
 		until deal_choice == 'exit'
 			puts "Choose a deal to see more details or back to main menu with 'exit'"
 			deal_choice = gets.strip.downcase
@@ -61,53 +61,12 @@ class FlightDeal::CLI
 				 answer = gets.strip.downcase
 				 if answer.include?('y')
 				 	system('open',"#{deal.url}")
-				 end
-				 deal_choice = nil
 				else
+					deal_choice = nil
 					puts "Sorry please try again."
-				end# if loop
-			end#until
-		end
-
+				end
+			end# if loop
+		end#until
 	end
+end
 				
-
-# 				else
-# 					puts "Sorry I didn't get that, please try again"
-# 				end
-# 			end
-# 		end
-
-# 		def deal_detail
-
-# 			#detail_choice = nil
-# 			until detail_choice == 'exit'
-# 				puts "Here are the deals details, type exit to go back"
-# 				detail_choice = gets.strip.downcase
-# 				if detail_choice == 'exit'
-# 					puts "Now going back to the city deals"
-
-# 				elsif 
-# 					deal = @deal_url
-
-# 				   @deal_details = FlightDeal::Scraper.new.scrape_details(deal)
-					
-						
-# 					# puts "#{detail_choice}. #{city.name}"
-# 					puts "Travel Date: #{detail.travel_date}"
-					
-# 				 	puts "----------------------------------------------------------------"
-# 				 	puts "Would you like to read this story in the browser?"
-# 				 	answer = gets.strip.downcase
-# 				 if answer.include?('y')
-# 				 	system('open', "#{deal.url}")
-# 				 end
-# 				 deal_choice = nil
-# 				end
-# 				# else 
-# 				# 	puts "sorry please try again."
-# 				# end
-			
-# 		end
-# 	end
-# end
--- lib/flight_deal/scraper.rb
+++ lib/flight_deal/scraper.rb
@@ -1,10 +1,7 @@
-require 'pry'
+
 class FlightDeal::Scraper
 
 BASE_URL = "http://www.theflightdeal.com/category/flight-deals/"
-
-
-
 
 	def scrape_cities
 		doc = Nokogiri::HTML(open("http://www.theflightdeal.com/category/flight-deals/"))
@@ -14,43 +11,25 @@ BASE_URL = "http://www.theflightdeal.com/category/flight-deals/"
 			 new_city.name = city.text
 			 new_city.search_id = city.attributes["href"].value.gsub("http://www.theflightdeal.com/category/flight-deals/", "")
 			 new_city.url = city.values
-
 		end
 	end
 	
 
 	def scrape_deals(city) #returns a list of deal hashes
-		
-			api_query = "#{BASE_URL + city.search_id.to_s}"
+			api_query = "#{BASE_URL + city.search_id}"
 			doc = Nokogiri::HTML(open(api_query))
 			 @city_deals = doc.search("h1.post-title").search("a").collect do |deal|
-				
-				new_deal = FlightDeal::Deal.new
-				new_deal.city = city
+			 	
+				new_deal = FlightDeal::Deal.new #creates new deal object
+				new_deal.city = city #object collab, creates instances of chosen cities @deals = [], @search_id, @url
 				new_deal.description = deal.text
 				new_deal.url =  deal.values.first
 				new_deal.save
-				city.deals << new_deal
-
-			end
-			city.deals
+				city.deals << new_deal #new_deal is saved into the @deals instance array
+				#saves the name, search_id, url for the city chosen into @city_deals instance			
+				end
+			city.deals #displays the cities attr with the chosen deals url for the city
 		end
-	
 
-	def scrape_details(deal)
-		
-		# @detail = detail.strip.downcase.gsub(/(&|&amp;)/, ' and ').gsub(/[\s\.\/\\]/, '-').gsub(/[^\w-]/, '').gsub(/[-_]{2,}/, '-').gsub(/^[-_]/, '').gsub(/[-_]$/, '')
-		# api_query = "#{BASE_URL_1 + } + #{@detail}"
-			doc = Nokogiri::HTML(open(deal))
-			@deal_details = doc.search(".entry-content").first.search("ul").collect do |detail|
-				
-				new_detail = FlightDeal::Deal.new
-				new_detail.deal = deal
-				new_detail.travel_date = detail.text
-				new_detail.save
-				deal.details << new_detail
-			end
-			deal.details
-		end
 	end
 
